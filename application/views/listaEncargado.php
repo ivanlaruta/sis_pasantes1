3941208332c01ebac88bee8d9c20869a129e81a4
Ashly Tintaya <Ashly Tintaya>
2016-10-06 07:52:23 -0400

    Age

---
+++ application/views/listaEncargado.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body background="fondo2.jpg" style='background-attachment: fixed;'>
+	
+<br>
+<div align="center">
+<table border="1">
+<tr>
+<td width="800" bgcolor="white">
+
+<div align="center">	
+<table border="0">
+<tr>
+<td>
+<img src="escudo.jpg" width="100" height="120">
+</td>
+<td>
+<h2><b>Universidad Católica Boliviana "San Pablo"</b></h2>
+
+<h3><b>Sistema de Control de Pasantes</b></h3>
+</td>
+</tr>
+</table>
+</div>
+<br>
+<div class="row-fluid">
+ <div class="span1" align="center">
+
+	<a href="" class="btn btn-default" style='width:160px'>UCB</a>
+	<a href="" class="btn btn-default" style='width:160px'>La Paz</a>
+	<a href="" class="btn btn-default" style='width:160px'>Sistema Académico</a>
+	<a href="" class="btn btn-default" style='width:160px'>UCB Mail</a> 	
+
+ </div>
+ </div>
+
+ <div align="center">
+ <h2><b>BIENVENIDO: Mauricio Fernandez</b></h2>
+</div>
+
+
+
+<div align="center">
+<table border="0">
+<tr>
+<td colspan="2">
+<center><h3>Listado de encargados</h3></center>
+</td>
+
+</tr>
+
+<div class="container">
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Id de encargado</th>        
+        <th>Carnet de identidad</th>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Direcciòn</th>        
+        <th>Telefono</th>
+        <th>E-mail</th>
+        <th>Usuario</th>
+        <th>Password</th>
+        <th>Rol</th>
+        <th>Titulo</th>
+        <th>Turno</th>
+
+      </tr>
+    </thead>
+
+    <tbody>
+      <td>1</td>        
+        <td>9092688</td>
+        <td>Marcelo</td>
+        <td>Fernandez</td>
+        <td>Calle 2 de Obrajes</td>
+        <td>23456789</td>
+        <td>marcelo@gmail.com</td>
+        <td>Marcelo</td>
+        <td>1234</td>
+        <td>Encargado</td>
+        <td>Ingeniero en Telecomunicaciones</td>
+        <td>Mañana</td>
+    </tbody>
+
+  </table>
+</div>
+</div>
+
+<hr width="100%" size="3px">
+<div align="right">
+	
+<font size="2">© 1998-2016 Universidad Católica Boliviana
+<br>
+Centro de Sistemas
+<br>
+email: csinf@ucb.edu.bo
+</font>
+</div>
+
+</td>
+</tr>
+
+</table>
+</div>
+
+
+</body>
+</html>
