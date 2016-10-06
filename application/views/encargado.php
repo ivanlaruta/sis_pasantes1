05be27ec3ff7155a8204672348871e9aebd5a81c
ivan.laruta <ivan.laruta@gmail.com>
2016-10-05 23:40:38 -0400

    subiendo bootstrap y vista inicial modelo

---
+++ application/views/encargado.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+</head>
+<body background="fondo2.jpg" style='background-attachment: fixed;'>
+	
+<br>
+<div align="center">
+<table border="1">
+<tr>
+<td width="800" bgcolor="white">
+
+<div align="center">	
+<table border="0">
+<tr>
+<td>
+<img src="escudo.jpg" width="100" height="120">
+</td>
+<td>
+<h2><b>Universidad Católica Boliviana "San Pablo"</b></h2>
+
+<h3><b>Sistema de Control de Pasantes</b></h3>
+</td>
+</tr>
+</table>
+</div>
+<br>
+<div class="row-fluid">
+ <div class="span1" align="center">
+
+	<a href="" class="btn btn-default" style='width:160px'>UCB</a>
+	<a href="" class="btn btn-default" style='width:160px'>La Paz</a>
+	<a href="" class="btn btn-default" style='width:160px'>Sistema Académico</a>
+	<a href="" class="btn btn-default" style='width:160px'>UCB Mail</a> 	
+
+ </div>
+ </div>
+
+ <div align="center">
+ <h2><b>BIENVENIDO: Carlos Perez</b></h2>
+
+</div>
+
+
+<div align="center">
+<table border="0">
+<tr>
+<td colspan="2">
+<center><h3>Menú Principal</h3></center>
+</td>
+
+</tr>
+
+<tr>
+<td height="60">
+	<a href="" class="btn btn-primary" style='width:170px'>Control de Pasantes</a>
+</td>
+
+<td>
+	Registro, Asignacion de Horarios y Tareas
+</td>
+</tr>
+<tr>	
+<td height="60">
+	<a href="" class="btn btn-primary" style='width:170px'>Horarios</a>
+	<br>
+</td>
+
+<td>
+
+	Verificar Horarios
+</td>
+</tr>
+<tr>
+<td height="60">
+	<a href="" class="btn btn-primary" style='width:170px'>Datos Personales</a>
+</td>
+
+<td>
+	Revisión de Datos Personales 
+</td>
+</tr>
+<tr>
+<td height="60">
+	<a href="" class="btn btn-primary" style='width:170px'>Reporte de Tareas</a>
+</td>
+
+<td>
+	Enviar Solicitudes Especiales o de Permiso
+</td>
+
+</tr>
+
+<tr>
+<td height="60">
+	<a href="" class="btn btn-primary" style='width:170px'>Observaciones</a>
+</td>
+
+<td>
+	Registrar Observaciones
+</td>
+
+</tr>
+
+<tr>
+<td height="60">
+	<a href="" class="btn btn-primary" style='width:170px'>Solicitudes Pendientes</a>
+</td>
+
+<td>
+	Verificar Solicitudes Pendientes
+</td>
+
+</tr>
+</table>
+</div>
+
+<hr width="100%" size="3px">
+<div align="right">
+	
+<font size="2">© 1998-2016 Universidad Católica Boliviana
+<br>
+Centro de Sistemas
+<br>
+email: csinf@ucb.edu.bo
+</font>
+</div>
+
+</td>
+</tr>
+
+</table>
+</div>
+
+
+</body>
+</html>
