d6387fa4a76d383e00f34fa7e80438d3a32eb3d8
Ashly Tintaya <Ashly Tintaya>
2016-10-12 10:06:15 -0400

    correcciones basicas

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tallersis/sis_pasantes/';
+$config['base_url'] = 'http://localhost/tallersis/sis_pasantes1/';
 
 /*
 |--------------------------------------------------------------------------
