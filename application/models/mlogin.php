acf7ccc79f0d54651cab40082e86a98005f721c3
luisdaniel123 <luigihbk@gmail.com>
2016-10-20 23:00:06 -0400

    agregando el modelo del login

---
+++ application/models/mlogin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* 
+*/
+class Mlogin extends CI_Model
+{
+	
+	public function ingresar($usu,$pass){
+		$this->db->select('b.id_rol, a.nombre,a.apellido,b.rol');
+		$this->db->from('rol b');
+		$this->db->join('persona a','a.id_rol=b.id_rol');
+		$this->db->where('a.usuario',$usu);
+		$this->db->where('a.password',$pass);
+
+
+		$resultado = $this->db->get();
+
+
+		if ($resultado->num_rows() == 1) {
+			$r = $resultado->row();
+
+			$s_usuario = array(
+				's_idusuario' => $r->id_rol,
+				's_usuario'  => $r->nombre.", ".$r->apellido. ", ".$r->rol
+				);
+
+			$this->session->set_userdata($s_usuario);
+			return 1;
+		}
+
+		else{
+
+			return 0;
+		}
+
+
+
+	}
+}
