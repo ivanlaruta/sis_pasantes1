79643a0b04bebecfb27677f94ebbe56f6c0aad63
luisdaniel123 <luigihbk@gmail.com>
2016-10-20 22:54:38 -0400

    agregando el controlador del login principal

---
+++ application/controllers/clogin.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* 
+*/
+class Clogin extends CI_Controller
+{ 
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->model('mlogin');
+	}
+
+
+	public function index(){
+		$data['mensaje']='';
+		$this->load->view('vlogin',$data);
+
+	}
+
+	public function ingresar(){
+
+		$usu = $this->input->post('txtUsuario');
+		$pass = $this->input->post('txtClave');
+
+		$res = $this->mlogin->ingresar($usu,$pass);  
+
+		if ($res == 1) {
+			$this->load->view('correcto');
+		}
+		else
+		{
+			$data['mensaje']="Usuario o Contraseña Incorrectos, Intente Nuevamente";
+			$this->load->view('vlogin',$data);
+
+		}
+	}
+}
