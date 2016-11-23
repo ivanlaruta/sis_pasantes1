bd46fa25d6aef60b520d2ae1005fd0c63406a2d3
NoeMachicao <noemachicao@gmail.com>
2016-11-22 22:40:09 -0500

    horarios 4

--- application/views/pasante/horarios.php
+++ application/views/pasante/horarios.php
@@ -1,20 +1,47 @@
       
 <body background="<?php echo base_url(); ?>images/fondo2.jpg" style='background-attachment: fixed;'>	
-	 	
-	<div align="center">
+ <div align="center">
+           
+       
+                
+      <div class ="login-card" >            
+                    <table border="0">
+                        <tr>
+                            <th >Dia</th>
+                            <td><input type="text" class="form-control" name="Nombre"  /></td>
+                        </tr>
+                        <tr>
+                            <th >Hora Inicio</th>
+                            <td><input type="text" class="form-control" name="Apellido"  /></td>
+                        </tr>
+                        
+                        <tr>
+                            <th >Hora Fin</th>
+                            <td><input type="text" class="form-control" name="FechaNacimiento"  /></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2">
+                                <button type="submit" class="btn btn-primary">Guardar</button>
+                            </td>
+                        </tr>
+                    </table>
+               </div> 
+<div class="container">
 
-<table  class="table table-bordered">
+                <table class="table table-bordered">
                     <thead>
                         <tr>
-                            <th style="text-align:left;">Dia</th>
-                            <th style="text-align:left;">Hora Inicio</th>
-                            <th style="text-align:left;">Hora Fin</th>
-                            <th style="text-align:left;">Detalle</th>
+                            <th>ID</th>
+                            <th>Dia</th>
+                            <th>Hora Inicio</th>
+                            <th>Hora Fin</th>
                            
                         </tr>
                     </thead>
-                    
+
                 </table>     
-</div>
+              
+            </div>
+        </div>
 
 </body>
