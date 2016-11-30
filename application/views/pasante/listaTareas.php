b059d8d49ea0803bf91130ba5eeff690f339b77b
MarielaTintaya <mn.tintaya@acad.ucb.edu.bo>
2016-11-30 11:53:58 -0400

    Lista de tareas

---
+++ application/views/pasante/listaTareas.php
@@ -0,0 +1,52 @@
+<body background="<?php echo base_url(); ?>images/fondo2.jpg" style='background-attachment: fixed;'>
+
+<div align="center">
+<br>
+ 	<h4>Lista de tareas:</h4>
+ 	<br>
+
+</div>
+
+
+<div align="center">
+		<table class="table table-bordered">
+			
+				<thead>
+					<th>Numero</th>
+					<th>Actividad</th>
+					<th>Fecha inicio</th>
+					<th>Editar</th>
+				</thead>
+
+				<tbody>
+					<?php
+					if(is_array($listarTareas) && !empty($listarTareas))
+					{
+						foreach ($listarTareas as $up ) 
+						{
+						?>
+						
+						<tr>
+							<td><?php echo $up -> id_actividad ?></td>
+							<td><?php echo $up -> actividad ?></td>
+							<td><?php echo $up -> fecha_inicio ?></td>
+							<td><a href="<?php echo base_url();?>index.php/controlIngresarEstadoPasante" class="btn btn-primary" style='width:160px'>Editar</a></td>
+							
+					<br>
+				</td>
+						</tr>
+						<?php
+						}
+					}
+
+					?>
+				</tbody>
+				
+		</table>
+
+</div>
+
+<div align="center">
+	<a href="<?php echo base_url();?>index.php/Jefe" class="btn btn-primary" style='width:160px'>aceptar</a>
+</div>
+</body>
