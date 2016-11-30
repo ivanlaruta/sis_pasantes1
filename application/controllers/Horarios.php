a71ad501253f97d9e9dcf486aa023c961dd67474
NoeMachicao <noemachicao@gmail.com>
2016-11-30 17:12:22 -0500

    Controlador

--- application/controllers/Horarios.php
+++ application/controllers/Horarios.php
@@ -1,16 +1,73 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class horarios extends CI_Controller {
 
-class Horarios extends CI_Controller {
-
-	function __construct()
+	public function __construct()
 	{
-		parent:: __construct();
+	    parent::__construct();
+	    $this->load->model("modelohorario");
 	}
-	public function index()
+
+	function index()
 	{
 		$this->load->view('header');
 		$this->load->view('pasante/horarios');
 		$this->load->view('footer');
 	}
+
+	function guardar(){
+		
+		if ($this->input->is_ajax_request()) {
+			$nombres = $this->input->post("pasante");
+			$dia = $this->input->post("dia");
+			$horai = $this->input->post("hora_inicio");
+			$horaf = $this->input->post("hora_fin");
+			
+
+			
+			if($this->modelohorario->guardar($datos)==true)
+				echo "Registro Guardado";
+			else
+				echo "No se pudo guardar los datos";
+		}
+		else
+		{
+			show_404();
+		}
+
+
+	}
+
+	function mostrar(){
+		if ($this->input->is_ajax_request()) {
+			$buscar = $this->input->post("buscar");
+			$datos = $this->modelohorario->mostrar($buscar);
+			echo json_encode($datos);
+			
+		}
+		else
+		{
+			show_404();
+		}
+	}
+
+	
+
+	function eliminar(){
+		if ($this->input->is_ajax_request()) {
+			$idsele = $this->input->post("id");
+			if($this->modelohorario->eliminar($idsele) == true)
+				echo "Registro Eliminado";
+			else
+				echo "No se pudo eliminar los datos";
+			
+		}
+		else
+		{
+			show_404();
+		}
+	}
+
 }
+
