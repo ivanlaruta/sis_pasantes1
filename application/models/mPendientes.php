45d6d5fa4ee70254d7ddb1575b16f4a13da35d15
luisdaniel123 <luigihbk@gmail.com>
2016-12-13 20:31:45 -0400

    agregando correciones al modelo, vista y controlador de solicitudes

--- application/models/mPendientes.php
+++ application/models/mPendientes.php
@@ -8,18 +8,43 @@ class mPendientes extends CI_Model
 
 
 
+public function getPost(){
 
-public function listarsolicitudes ()
-	{
-		$this->db->select('cat_tipo_permiso,cat_estado_per,fecha,descripcion');
-		$this->db->from('permiso');
-			
+	return $this->db->get('permiso','persona','pasante');
 
-		$resultado = $this->db->get();
-		
-		if ($resultado->num_rows()>0) {
-			 return $resultado -> result();
-		}
-	}
+
+
+}
+
+//$cat_carrera,$nombres,$apellidos
+public function getPostByName($cat_carrera,$nombres,$apellidos)
+{
+
+
+
+		$this->db->select('f.fecha,f.cat_estado_per, c.cat_carrera, n.nombres,n.apellidos');
+		$this->db->from('permiso f');
+		$this->db->join('pasante c','f.id_pasante=c.id_pasante');
+		$this->db->join('persona n','n.id_persona=c.id_persona');
+		$this->db->where('c.cat_carrera',$cat_carrera);
+		$this->db->where('n.nombres',$nombres);
+		$this->db->where('n.apellidos',$apellidos);
+
+
+//$this->db->query("SELECT f.fecha,f.cat_estado_per, c.cat_carrera, n.nombres,n.apellidos
+//FROM permiso f
+//inner join pasante c on f.id_pasante=c.id_pasante
+//inner join persona n on n.id_persona=c.id_persona
+//WHERE c.cat_carrera = '".$cat_carrera."'
+//WHERE n.nombres = '".$nombres."'
+//WHERE n.apellidos = '".$apellidos"'
+
+//");
+
+return $result->row();
+
+}
+
+
 
 }
